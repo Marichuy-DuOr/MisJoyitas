ef4470371e289dc1b2f09f8590cea968761622bc
Mary Chuy <maria.0299.ornelas@gmail.com>
2020-07-06 03:30:56 -0500

    Inventario terminado

--- src/app/services/firestore.service.ts
+++ src/app/services/firestore.service.ts
@@ -64,6 +64,51 @@ export class FirestoreService {
   public getVentas() {
     return this.firestore.collection('ventas').snapshotChanges();
   }
+
+  public Compra(data: {
+    cantidad: number,
+    idProducto: string
+  }) {
+    return this.firestore.collection('compras').add(data);
+  }
+
+  public getCompra(documentId: string) {
+    return this.firestore.collection('compras').doc(documentId).snapshotChanges();
+  }
+
+  public getCompras() {
+    return this.firestore.collection('compras').snapshotChanges();
+  }
+
+  public updateCompra(documentId: string, data: any) {
+    return this.firestore.collection('compras').doc(documentId).set(data);
+  }
+
+  public deleteCompra(documentId: string) {
+    return this.firestore.collection('compras').doc(documentId).delete();
+  }
+
+  public Existencias(data: {
+    id: string,
+    cantidad: number,
+    idProducto: string,
+    imagen: string,
+    nombre: string
+  }) {
+    return this.firestore.collection('existencias').add(data);
+  }
+
+  public getExistencias() {
+    return this.firestore.collection('existencias').snapshotChanges();
+  }
+
+  public getExistencia(documentId: string) {
+    return this.firestore.collection('existencias').doc(documentId).snapshotChanges();
+  }
+
+  public updateExistencia(documentId: string, data: any) {
+    return this.firestore.collection('existencias').doc(documentId).set(data);
+  }
 }
 
 interface DatosVenta {
